5c12fa68b2295c76e5a748f97f2be578da602e79
MingRone08 <yuenbinhminh@gmail.com>
2019-09-16 09:58:25 +0700

    Delete wrong path files

--- Demo2/check_session.php
+++
@@ -1,7 +0,0 @@
-<?php
-    if($_SESSION['logged_in'] == 1) {
-        header('Location: http://localhost:8080/Work/Demo2/list.php',true,301);
-    } else if($_SESSION['logged_in'] == 0){
-        header('Location: http://localhost:8080/Work/Demo2/login.php',true,301);
-    }
-?>
--- Demo2/login.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	if(isset($_POST['submit'])){
-		$_SESSION['name'] = $_POST['uname'];
-		$_SESSION['logged_in'] = 1;
-		include ('check_session.php');
-	}
-
-	include ($_SERVER['DOCUMENT_ROOT'].'/Work/Demo2/Style/html/login.html');
-?>
