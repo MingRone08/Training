09857da3f7a008f37f69793b8779d1a0776936c3
MingRone08 <yuenbinhminh@gmail.com>
2019-09-27 11:38:40 +0700

    Finished product

--- php-mvc/App/Controllers/Home.php
+++ php-mvc/App/Controllers/Home.php
@@ -19,57 +19,61 @@ class Home extends \Core\Controller
    *
    * @return void
    */
-  public function indexAction()
-  {
-  	$productList = User::getAll();
-
-    View::render('Home/index.phtml', $productList);
-  }
-
-  public function addAction()
-  {
-    if (!isset($_POST['submit']))
+    public function indexAction()
     {
-      View::render('Home/form-add-product.phtml');
+        $productList = User::getAll();
+
+        View::render('Home/index.phtml', $productList);
     }
-    else
+
+    public function addAction()
     {
-     $target = $target = "image/".basename($_FILES['image']['name']);
-     $image = $_FILES['image']['name'];
-     move_uploaded_file($_FILES['image']['tmp_name'], $target);
-     User::addProduct($_POST['Product_name'], $_POST['Price'], $image);
-     header("location:Home",true,301);
-    }	
-  }
-
-  public function deleteAction()
-  {
-    User::deleteProduct($_SESSION['params']);
-    header("location:Home",true,301);
-  }
-
-  public function filterAction()
-  {
-    if(!isset($_POST['submit'])){
-      View::render('Home/filter-form.phtml');
-    }else{
-      $result = User::filterProduct();
-        //var_dump($result);
-      View::render('Home/index.phtml', $result);
+        if (!isset($_POST['submit']))
+        {
+            View::render('Home/form-add-product.phtml');
+        }
+        else
+        {
+            $target = $target = "image/".basename($_FILES['image']['name']);
+            $image = $_FILES['image']['name'];
+            move_uploaded_file($_FILES['image']['tmp_name'], $target);
+            User::addProduct($_POST['Product_name'], $_POST['Price'], $image);
+            header("location:Home",true,301);
+        }	
     }
-  }
 
-  public function editAction()
-  {
-    if(!isset($_POST['edited']))
+    public function deleteAction()
     {
-      $result = User::getProduct($_SESSION['params']);
-        //var_dump($result);
-      View::render('Home/edit-form.phtml', $result);
-    }else
-    {
-      User::editProduct();
-      header("location:Home",true,301);
+        User::deleteProduct($_SESSION['params']);
+        header("location:Home",true,301);
     }
-  }
+
+    public function filterAction()
+    {
+        if(!isset($_POST['submit']))
+        {
+            View::render('Home/filter-form.phtml');
+        }
+        else
+        {
+            $result = User::filterProduct();
+            //var_dump($result);
+            View::render('Home/index.phtml', $result);
+        }
+    }
+
+    public function editAction()
+    {
+        if(!isset($_POST['edited']))
+        {
+            $result = User::getProduct($_SESSION['params']);
+            //var_dump($result);
+            View::render('Home/edit-form.phtml', $result);
+        }
+        else
+        {
+            User::editProduct();
+            header("location:Home",true,301);
+        }
+    }
 }
